4d9306a1439a6aaad0fb10ac7072d51495ecf640
tomohiro1024 <gongyezhihong732@gmail.com>
2023-11-20 20:24:51 +0900

    ある程度完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { type } from 'os';
 
 function App() {
 
+  // inputValueはテキストフィールドに入力した値
+
   const [inputValue, setInputValue] = useState("")
   const [todos, setTodos] = useState<Todo[]>([])
 
@@ -13,14 +15,59 @@ function App() {
     checked: boolean;
   }
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setInputValue(e.target.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // リロード回避
+    e.preventDefault()
+    // 新しいTODOの作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    }
+    setTodos([newTodo, ...todos])
+    // 投稿した後は空にする
+    setInputValue("")
+  }
+
+  // 編集
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.inputValue =inputValue
+      }
+      return todo;
+    })
+    setTodos(newTodos)
+  }
+
+  const handleDelete = (id: number) => {
+    //idが正しくないのは残す。正しいと消す。
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>TODO</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" placeholder="タスクを入力" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className="inputText" placeholder="タスクを入力" />
           <input type="submit" value="Add" className="submitButton" />
         </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            // mapを使っているので、todosのデータが5つあった場合はliタグが5つ生成させる
+            <li key={todo.id}>
+              {todo.inputValue}
+              <button onClick={() => handleDelete(todo.id)}>消</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
